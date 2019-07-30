b2bc0b3b04d30d646846b3e297004e27af4259aa
satish <satishmalkar94@gmail.com>
2019-07-30 10:19:35 +0530

    Features::add recipe name and link

--- demo.rb
+++ demo.rb
@@ -1,50 +1,102 @@
-
 require 'nokogiri'
 require 'httparty'
-# require 'sqlite3'
-
-# begin
-
-#     db = SQLite3::Database.new ":memory:"
-#     puts db.get_first_value 'SELECT SQLITE_VERSION()'
-
-# rescue SQLite3::Exception => e
-
-#     puts "Exception occurred"
-#     puts e
-
-# ensure
-#     db.close if db
-# end
+require 'rubygems'
+require 'open-uri'
 
 class Scrapp
+  def initialize
+    @all_category = []
+    @all_recipes  =[]
+    # @doc = getHtml("https://www.allrecipes.com")
+    @doc = Nokogiri::HTML(open("https://www.allrecipes.com/"))
+    categories
+    parse_recipes
 
-def initialize
-@doc = getHtml("https://www.allrecipes.com")
+  end
+
+  def categories
+    puts 'get categories'
+    categoryName = @doc.to_html.scan(/<span class=\"category-title\" data-ellipsis>(.*)<\/span>/)
+    category_link =  @doc.to_html.scan(/<a\s*data-internal-referrer-link=\"top\s*hubs\"\s*href=\"(.*?)\"/)
+    category_names = []
+    @category_links = []
+
+    categoryName.each do |cat_name|
+            category_names.push(cat_name)
+    end
+
+    category_link.each do |cat_link|
+        @category_links.push(cat_link)
+    end
+
+    category_names.each_with_index do |cat_name,index|
+      rec_hash ={
+                  name: cat_name,
+                  link: @category_links[index]
+                }
+      @all_category.push(rec_hash)
+    end
+
+    puts @all_category
+    parse_recipes
+  end
+
+  def parse_recipes
+    @all_category.each do |i|
+        puts i[:link]
+      cat_link = Nokogiri::HTML(open(i[:link].join("")))
+      # cat_links = cat_link.join("")
+      rec_name = cat_link.to_html.scan(/<span class="fixed-recipe-card__title-link">(.*)<\/span>/)
+      rec_link = cat_link.to_html.scan(/<h3\s*class=\"fixed-recipe-card__h3\">\s*<a\s*href=\"(.*?)\"/)
+      rec_names = []
+      rec_links = []
+      i = 0
+      j = 0
+      # puts rec_name
+      rec_name.each do |rname|
+        puts rname
+        if i <= 9
+            rec_names.push(rname)
+            i = i + 1
+        end
+      end
+      puts rec_names
+
+      rec_link.each do |link|
+        if j <= 9
+          rec_link.push(link)
+          j = j + 1
+        end
+      end
+      puts rec_link
+
+      rec_names.each_with_index do |rname ,index|
+        rec_hash = {
+                    name: rname,
+                    link: rec_links[index]
+                    }
+                    @all_recipes.push(rec_hash)
+      end
+    end
+    puts @all_recipes
+  end
+
+def recipe_info
+  puts recipe_info
+
+
 end
 
-def execute
- categories = getcategories(@doc)
- puts categories
-end
 
-
-def getcategories(htmlcode)
-  # categoryNames1 = htmlcode.scan(/(?m)<div\sid=\"insideScroll\"\sclass=\"grid\sslider\">(.*)<\/div>/)
-
-  categoryName =htmlcode.scan(/<span class=\"category-title\" data-ellipsis>(.*)<\/span>/)
-  category_link = htmlcode.scan(/(?<=href\=)(["'])(.*?)\1(.*)(?=class="grid-col--subnav">)/)
-  recipe_name = htmlcode.scan(/(?<=(<span class="fixed-recipe-card__title-link">))(.*)(?=(<\/span>))/)
-
-end
-
-
-def getHtml(url)
-    HTTParty.get(url)
-end
-
+  def getHtml(url)
+      HTTParty.get(url)
+  end
 end
 srap = Scrapp.new
-srap.execute
+
+
+
+
+
 
 
